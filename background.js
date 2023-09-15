1df47c3cb13399d8a89024c58852f9843181bdb4
Zynkah <ffgurl07@yahoo.com>
2023-09-15 09:01:55 -0500

    updates

--- background.js
+++ background.js
@@ -2,45 +2,48 @@
 let isEnabled = false;
 
 // Listen for messages from the popup.js and content.js
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    // Check if the message is from the popup to toggle the extension state
-    if (request.toggleExtension !== undefined) {
-        isEnabled = request.toggleExtension;
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  // Check if the message is from the popup to toggle the extension state
+  if (request.toggleExtension !== undefined) {
+    isEnabled = request.toggleExtension;
 
-        // Send the new state to content scripts
-        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-            if (tabs[0]) {
-                const tabId = tabs[0].id;
-                chrome.scripting.executeScript({
-                    target: { tabId: tabId },
-                    function: setExtensionState,
-                    args: [isEnabled]
-                });
-            }
+    // Send the new state to content scripts
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      if (tabs[0]) {
+        const tabId = tabs[0].id;
+        chrome.scripting.executeScript({
+          target: { tabId: tabId },
+          function: setExtensionState,
+          args: [isEnabled],
         });
-    }
+      }
+    });
+  }
 });
 
 // A function to send the current extension state to content scripts
 function setExtensionState(isEnabled) {
-    chrome.runtime.sendMessage({ isEnabled: isEnabled });
+  chrome.runtime.sendMessage({ isEnabled: isEnabled });
 }
 
 // Initialize the context menu item to reset the extension state
 chrome.contextMenus.create({
-    id: "resetExtension",
-    title: "Reset Extension",
-    contexts: ["browser_action"]
+  id: "resetExtension",
+  title: "Reset Extension",
+  contexts: ["browser_action"],
 });
 
 // Add a click event listener to the context menu item
-chrome.contextMenus.onClicked.addListener(function(info, tab) {
-    if (info.menuItemId === "resetExtension") {
-        isEnabled = false; // Reset the extension state
-        chrome.storage.sync.set({ 'isEnabled': isEnabled });
-        chrome.runtime.sendMessage({ isEnabled: isEnabled }); // Notify content scripts
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+  if (info.menuItemId === "myContextMenuId") {
+    console.log("Clicked on menu item in tab: " + tab.url);
+  }
+  if (info.menuItemId === "resetExtension") {
+    isEnabled = false; // Reset the extension state
+    chrome.storage.sync.set({ isEnabled: isEnabled });
+    chrome.runtime.sendMessage({ isEnabled: isEnabled }); // Notify content scripts
 
-        // Update the popup's toggle switch
-        chrome.runtime.sendMessage({ updatePopup: isEnabled });
-    }
+    // Update the popup's toggle switch
+    chrome.runtime.sendMessage({ updatePopup: isEnabled });
+  }
 });
--- content.js
+++ content.js
@@ -1,47 +1,60 @@
 // Listen for messages from the background.js
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.isEnabled !== undefined) {
-        if (request.isEnabled) {
-            // Enable URL hiding
-            hideSpecificURLs();
-        } else {
-            // Disable URL hiding
-            showAllURLs();
-        }
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.isEnabled !== undefined) {
+    if (request.isEnabled) {
+      // Enable URL hiding
+      hideSpecificURLs();
+    } else {
+      // Disable URL hiding
+      showAllURLs();
     }
+  }
 });
 
 // Function to hide specific URLs
 function hideSpecificURLs() {
-    const results = document.querySelectorAll('.g'); // Assuming search results are in class "g"
+  const results = document.querySelectorAll(".g"); // Assuming search results are in class "g"
 
-    // Replace specific URLs you want to hide with an empty string
-    results.forEach(function(result) {
-        const link = result.querySelector('a');
-        if (link) {
-            const url = link.href;
-            
-            // Add conditions to identify and hide specific URLs
-            if (shouldHideURL(url)) {
-                result.style.display = 'none';
-            }
-        }
-    });
+  // Replace specific URLs you want to hide with an empty string
+  results.forEach(function (result) {
+    const link = result.querySelector("a");
+    if (link) {
+      const url = link.href;
+
+      // Add conditions to identify and hide specific URLs
+      if (shouldHideURL(url)) {
+        result.style.display = "none";
+      }
+    }
+  });
 }
 
 // Function to show all URLs
 function showAllURLs() {
-    const results = document.querySelectorAll('.g');
+  const results = document.querySelectorAll(".g");
 
-    results.forEach(function(result) {
-        result.style.display = 'block'; // Display all results
-    });
+  results.forEach(function (result) {
+    result.style.display = "block"; // Display all results
+  });
 }
 
-// Define conditions to identify specific URLs to hide
 function shouldHideURL(url) {
-    // Add your conditions here to specify which URLs to hide
-    // For example, you can check for keywords, domains, or other criteria
-    // Example: return url.includes('example.com');
+  // Get the list of all open tabs
+  chrome.tabs.query({}, function (tabs) {
+    if (chrome.runtime.lastError) {
+      console.error(chrome.runtime.lastError);
+      return false; // Handle the error as needed
+    }
+
+    // Iterate through the open tabs and check if the URL matches any of them
+    for (let i = 0; i < tabs.length; i++) {
+      if (tabs[i].url === url) {
+        // If a matching URL is found in an open tab, return true to hide it
+        return true;
+      }
+    }
+
+    // If no matching URL is found, return false to not hide it
     return false;
+  });
 }
